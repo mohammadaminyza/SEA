36f38a2ec63552f9037830e3edb023993a8ce907
60146 <60146@DEV-19>
2024-02-01 16:09:36 +0330

    add update validation

---
+++ src/Backend/01. Core/Arch.Core.ApplicationService/Orders/Commands/UpdateOrderAddress/UpdateOrderAddressValidator.cs
@@ -0,0 +1,18 @@
+﻿using Arch.Core.Contracts.Orders.Commands.UpdateOrderAddress;
+using Arch.Core.Domain.Common.Resources;
+using FluentValidation;
+using Zamin.Extensions.Translations.Abstractions;
+
+namespace Arch.Core.ApplicationService.Orders.Commands.UpdateOrderAddress;
+
+public class UpdateOrderAddressValidator : AbstractValidator<UpdateOrderAddressCommand>
+{
+    public UpdateOrderAddressValidator(ITranslator translator)
+    {
+        RuleFor(o => o.Street)
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(UpdateOrderAddressCommand.Street)]);
+
+        RuleFor(o => o.Plaque)
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(UpdateOrderAddressCommand.Plaque)]);
+    }
+}
