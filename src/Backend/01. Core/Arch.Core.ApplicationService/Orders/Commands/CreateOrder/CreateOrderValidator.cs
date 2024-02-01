cf492b177d6ea14146ce6ebd21ed6675fa022680
60146 <60146@DEV-19>
2024-02-01 15:13:50 +0330

    Add Translator Sloution

--- src/Backend/01. Core/Arch.Core.ApplicationService/Orders/Commands/CreateOrder/CreateOrderValidator.cs
+++ src/Backend/01. Core/Arch.Core.ApplicationService/Orders/Commands/CreateOrder/CreateOrderValidator.cs
@@ -10,18 +10,18 @@ public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
     public CreateOrderValidator(ITranslator translator)
     {
         RuleFor(o => o.Street)
-            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED]);
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(CreateOrderCommand.Street)]);
 
         RuleFor(o => o.Plaque)
-            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED]);
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(CreateOrderCommand.Plaque)]);
 
         RuleFor(o => o.UserId)
-            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED]);
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(CreateOrderCommand.UserId)]);
 
         RuleFor(o => o.OrderDetail.ProductId)
-            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED]);
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(CreateOrderCommand.OrderDetail.ProductId)]);
 
         RuleFor(o => o.OrderDetail.Count)
-            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED]);
+            .NotEmpty().WithMessage(translator[ArchValidationError.VALIDATION_ERROR_REQUIRED, nameof(CreateOrderCommand.OrderDetail.Count)]);
     }
 }
